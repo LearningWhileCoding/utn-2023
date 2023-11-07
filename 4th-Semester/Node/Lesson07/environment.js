9500cd9c7a69667265eb472a35137bdc46e756d7
Augusto <augustobritos95@gmail.com>
2023-11-06 22:41:47 -0300

    End of Lesson 07

---
+++ 4th-Semester/Node/Lesson07/environment.js
@@ -0,0 +1,6 @@
+let name = process.env.NAME || 'not a name';
+let web = process.env.WEB || 'not a web';
+console.log('Hello ' + name);
+console.log('My web is: ' + web);
+
+console.log('Last execution');
